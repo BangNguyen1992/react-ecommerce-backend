300b5199b929d9e09ee6052fcba8b8b682c2da25
Bang Nguyen <nguyenbahaibang@gmail.com>
2020-03-08 19:33:41 +0200

    Bugfix graphQL playground cannot connect to server

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 require("dotenv").config({ path: "variables.env" });
 const createServer = require("./createServer");
 const db = require("./db");
+
 const server = createServer();
 
 // TODO: use express middleware to handle cookies (JWT)
@@ -12,7 +13,7 @@ server.start(
   {
     cors: {
       credentials: true,
-      origin: process.env.FRONTEND_URL
+      origin: [process.env.FRONTEND_URL, process.env.PLAYGROUND_URL]
     }
   },
   deets => {
