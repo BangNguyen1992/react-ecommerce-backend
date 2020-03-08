b225afea0b27beaa2f3a6d6bd3ff0b2dcaa791de
Bang Nguyen <nguyenbahaibang@gmail.com>
2020-03-08 20:21:56 +0200

    Add mutation createItem

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,3 +1,14 @@
-const Mutation = {};
+const Mutation = {
+  // TODO: Need to check if user logged in
+
+  createItem(parent, args, context, info) {
+    return context.db.mutation.createItem(
+      {
+        data: { ...args }
+      },
+      info
+    );
+  }
+};
 
 module.exports = Mutation;
