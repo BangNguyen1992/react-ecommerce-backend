4f9eff6a20609f4f68c1615f92090717e3471ef4
BangNguyen <nguyenbahaibang@gmail.com>
2022-04-07 02:46:12 +0300

    Add ProductImage schema

--- keystone.ts
+++ keystone.ts
@@ -4,6 +4,7 @@ import { createAuth } from '@keystone-next/auth';
 import { withItemData, statelessSessions } from '@keystone-next/keystone/session';
 import { User } from './schemas/User';
 import { Product } from './schemas/Product';
+import { ProductImage } from './schemas/ProductImage';
 
 const databaseURL = process.env.DATABASE_URL;
 
@@ -39,6 +40,7 @@ export default withAuth(
       // TODO: Add schema items
       User,
       Product,
+      ProductImage,
     }),
     ui: {
       // TODO: Change this for roles
